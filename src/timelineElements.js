38af2a9f0643d2b68bcbc722d827e79841f0a1e1
GRVLigiardi <gilleslig@gmail.com>
2022-08-30 20:11:18 +0200

    initial commit

---
+++ src/timelineElements.js
@@ -0,0 +1,58 @@
+let timelineElements = [
+  {
+    id: 1,
+    title: "Frontend Developer",
+    location: "Rosario, Argentina",
+    description:
+      "Starting my journey in development. Compiling courses from Microsoft Learn, UDEMY, MDN Web Docs and building projects. I've learned to build React Apps, WebSite and more. I started with JavaScript and I'm ready to work in a Front-End team. And willing to learn much more.",
+    date: "May 2022 - Infinity and Beyond",
+    icon: "school",
+  },
+  {
+    id: 2,
+    title: "Pack OFFICE Specialist",
+    location: "Rosario, Argentina",
+    description:
+      "Working hand-in-hand with small businesses, entrepreneur and micro business. Building Excel Sheets for accounting / stock managements / clients data bases. Building PowerPoint presentations for medical keynotes or political congresses.",
+    date: "May 2021 - Present",
+    icon: "work",
+  },
+  {
+    id: 3,
+    title: "Move from France to Argentina",
+    location: "Rosario, Argentina",
+    description:
+      "In december 2020 i moved from France to Argentina. As an adaptable, active listener person, In few months I learned spanish and the Argentinian culture.",
+    date: "December 2020",
+    icon: "school",
+  },
+  {
+    id: 4,
+    title: "Producer Morning News Show",
+    location: "Paris, France",
+    description:
+      "I had the chance to produce the morning news show of RFI radio, working with the bests journalists of France. I also produced and edited radio-documentaries and radio-fictions.",
+    date: "August 2018 - September 2020",
+    icon: "work",
+  },
+  {
+    id: 5,
+    title: "Producer / Director Entertainment medias ",
+    location: "Paris, France",
+    description:
+      "At my 19 years old I moved to Paris and went to a radio school, I had my diploma in 2012. I started at the bottom of the chain and grow to Artistic Director of a Tv Show in 'NRJ 12'. I was the youngest producer in a national radio when I started in 2013 at 'Virgin Radio France'. All those years I've learned to managed teams, to analyse ratings, understand and apply the client needs",
+    date: "2013 - 2018",
+    icon: "work",
+  },
+  {
+    id: 6,
+    title: "Marketing & Economy diploma",
+    location: "Nice, France",
+    description:
+      "Highschool with speciality : Marketing / Economy and accounting.",
+    date: "2007",
+    icon: "school",
+  },
+];
+
+export default timelineElements;
